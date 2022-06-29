0e9f4eddc26c3e302387fbda060fe5853ddde524
Marina <MI_BATAEVA@protek.ru>
2022-06-29 22:46:02 +0300

    create new function

--- index.ts
+++ index.ts
@@ -1,5 +1,26 @@
 import axios from "axios";
+import { title } from "process";
+interface ToDo{
+    id: number,
+    title: string,
+    completed: boolean
+}
+
 const url = 'http://jsonplaceholder.typicode.com/todos/1'
 axios.get(url).then(response=>{
-    console.log(response.data)
+    const todo = response.data as ToDo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    logTodo(id, title,completed)
+    
 })
+
+const logTodo = (id: number, title: string, completed: boolean)=>{
+    console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished: ${completed}`)
+}
